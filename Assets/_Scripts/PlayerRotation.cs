24dde835c0c8b4737933c126e9c436d17947acb5
Devin Sturgeon <dwsturge@gmail.com>
2025-04-11 18:57:17 -0400

    Unity Setup
    Setup

---
+++ Assets/_Scripts/PlayerRotation.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace TopDown.Movement
+{
+    public class PlayerRotation : Rotator
+    {
+
+        private void OnLook(InputValue value)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint( value.Get<Vector2>());
+            LookAt( mousePosition );
+        }
+    }
+}
---
+++ Assets/_Scripts/Rotator.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace TopDown.Movement
+{
+    public class Rotator : MonoBehaviour
+    {
+        protected void LookAt(Vector3 target)
+        {
+            //Calculate angle between transform and target
+            float lookAngle = AngleBetweenTwoPoints(transform.position, target) + 90;
+
+            //Assign the target rotation on the Z axis
+            transform.eulerAngles = new Vector3(0, 0, lookAngle);
+        }
+
+        private float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+        {
+            return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+        }
+    }
+}
