85378e6711f7ed9724549a6f767ddf77221d7ee2
Devin Sturgeon <dwsturge@gmail.com>
2025-04-18 06:08:58 -0400

    enemy prefab
    added movement and rotation.
    broke the bullet spawner

--- Assets/_Scripts/EnemyController.cs
+++ Assets/_Scripts/EnemyController.cs
@@ -1,23 +1,22 @@
 using System;
 using System.Collections;
 using System.Data.Common;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    private Vector3 playerPosition;
-    private Vector3 enemyPosition;
     private GameObject player;
     private Transform playerTransform;
-    private Transform enemyTransform;
 
     [Header("Variables")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float rotationSpeed;
     [SerializeField] private float addedRotation;
-
+    
     public static EnemyController instance;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     private void Awake()
     {
         if (instance == null)
@@ -26,33 +25,38 @@ public class EnemyController : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
         //get player obj
         player = GameObject.FindGameObjectWithTag("Player");
-
-        //get player transform for position
-        Transform playerTransform = player.GetComponent<Transform>();
-
-        getPlayerPosition();
-        rotateEnemyTowardsPlayer();
     }
 
-    private void getPlayerPosition()
+
+    void Update()
     {
-        playerPosition = new Vector3(playerTransform.position.x, playerTransform.position.y, 0);
+        //get player transform for position
+        playerTransform = player.GetComponent<Transform>();
+        
+        rotateEnemyTowardsPlayer();
+        moveTwoardsPlayer();
     }
+
 
 
     private void rotateEnemyTowardsPlayer()
     {
-        Vector2 direction = (playerPosition - transform.position).normalized;
+       Vector3 direction = (playerTransform.position - transform.position).normalized;
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + addedRotation;
         Quaternion targetRotation = Quaternion.Euler(0f, 0f, angle);
 
         //if you are reading this psy, this smooths the rotation.
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed);
+    }
+
+    private void moveTwoardsPlayer()
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, moveSpeed);
     }
 }
